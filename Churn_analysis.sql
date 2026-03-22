4a0d0bb1f5ad81d0202f73e897d8fd4b8d299a40
Monish-2305 <106082164+Monish-2305@users.noreply.github.com>
2026-03-22 09:20:33 +0530

    Update Churn_analysis.sql

--- Churn_analysis.sql
+++ Churn_analysis.sql
@@ -1,1 +1,71 @@
+--Customer Churn Event Analysis
+
+--1. Total Churn Events
+SELECT COUNT(*) AS Churn_Events
+FROM ravenstack_churn_events
+
+--2. Unique accounts that churned
+SELECT COUNT (DISTINCT account_id) AS unique_churn_accounts
+FROM ravenstack_churn_events
+
+--3.Monthly Churn Trend
+SELECT
+DATE_TRUNC(churn_date, month) AS churn_month,
+COUNT(DISTINCT account_id) AS Churned_accounts
+FROM ravenstack_churn_events
+GROUP BY churn_month
+ORDER BY churn_month
+
+--4. Revenue Impact Analysis
+--Total Revenue Lost
+SELECT 
+SUM (refund_amount_usd) AS Total_refund_loss
+FROM ravenstack_churn_events
+
+--Average Refund Per Account
+SELECT
+AVG(refund_amount_usd) AS average_refund_per_account
+FROM ravenstack_churn_events
+
+--Revenue Loss By Month
+SELECT
+DATE_TRUNC(churn_date, month) AS churn_month,
+ROUND (SUM (refund_amount_usd),2) AS refund_loss
+FROM ravenstack_churn_events
+GROUP BY churn_month 
+ORDER BY churn_month DESC
+
+--5. Root Cause Analysis
+--Top Churn Reason
+SELECT 
+reason_code,
+Count(*) AS churn_count
+FROM ravenstack_churn_events
+GROUP BY reason_code
+ORDER BY churn_count DESC
+  
+--Revenue Loss by Churn Reason
+SELECT
+reason_code,
+ROUND (SUM(refund_amount_usd),2) AS refund_loss,
+FROM data-analytics-464302.Ravenstack_churnevents.Ravenstack_churn
+GROUP BY reason_code
+ORDER BY refund_loss DESC
+
+--6. Upgrade/Downgrade signals
+--Percentage of people downgraded before churn
+SELECT 
+preceding_downgrade_flag,
+COUNT (*) AS churn_count,
+ROUND(COUNT (*) *100 / SUM (COUNT(*)) OVER(), 2) AS PERCENTAGE
+FROM data-analytics-464302.Ravenstack_churnevents.Ravenstack_churn
+GROUP BY preceding_downgrade_flag
+
+--Percentage of People Ugraded before churn
+SELECT
+preceding_upgrade_flag,
+COUNT (*) AS churn_count,
+ROUND(COUNT (*) *100 / SUM (COUNT(*)) OVER(), 2) AS PERCENTAGE
+FROM data-analytics-464302.Ravenstack_churnevents.Ravenstack_churn
+GROUP BY preceding_upgrade_flag
 
